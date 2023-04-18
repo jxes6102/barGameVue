6eb108cc6955a91478e299c83dc427caf5adc2de
jxes6102 <jxes6102@gmail.com>
2023-04-18 13:58:55 +0800

    temp04181400

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -15,29 +15,23 @@
                 <!--出口-->
                 <div class="absolute w-[80px] h-[80px] left-[100px] top-[325px] md:w-[136px] md:h-[138px] md:left-[285px] md:top-[580px] z-[1]"><img src="@/assets/images/mendong.png"></div>
                 <!--掉落物-->
-                <!-- <div class="zjdl " :class="fallStatus ? 'dila_Y' : ''">
+                <div class="absolute w-[130px] h-[110px] left-[290px] top-[600px] z-[2] flex flex-wrap justify-center items-center" :class="fallStatus ? 'dila_Y' : ''">
                     <span :data-content="fallNum" :class="fallStatus ? 'diaL_one' : ''"></span>
-                </div> -->
+                </div>
             </div>
             <!--號碼訊息-->
-            <!-- <div class="zonj_zezc" id="jianpin_one" v-show="messageStatus">
-                <div class="jpzs aiqiyi tc_anima">
-                    <em><img src="@/assets//images/close.png" @click="close"></em>
-                    <h2 id="message" v-html="messageText"></h2>
-                </div>
-            </div> -->
-            <div 
-                class="bg-[#6f8fea] absolute w-[270px] h-[270px] md:w-[500px] md:h-[500px] left-[calc(50%-135px)] top-[calc(50%-135px)] md:left-[calc(50%-250px)] md:top-[calc(50%-250px)] z-[4] flex flex-wrap justify-center items-center" 
-                id="jianpin_one" v-show="true"
+            <div
+                class="absolute w-[270px] h-[270px] md:w-[500px] md:h-[500px] left-[calc(50%-135px)] top-[calc(50%-135px)] md:left-[calc(50%-250px)] md:top-[calc(50%-250px)] z-[4] flex flex-wrap justify-center items-center" 
+                id="jianpin_one" v-show="messageStatus"
             >
-                <div class="tc_anima absolute w-[250px] h-[175px] md:w-[450px] md:h-[286px] rounded-[10px] bg-[#fff] ">
+                <div class="tc_anima absolute w-[250px] h-[175px] md:w-[450px] md:h-[286px] rounded-[10px] bg-[#fff] py-2">
                     <em class="absolute h-[95px] right-0 top-[-40px] md:top-[-50px]">
                         <img src="@/assets/images/close.png" @click="close">
                     </em>
                     <h2 
                         class="text-[36px] text-[#ff1a4b] text-center leading-[40px] flex flex-wrap justify-center items-center break-all"
                         id="message" 
-                        v-html="'gggkdsf'"
+                        v-html="messageText"
                     >
                     </h2>
                 </div>
@@ -52,7 +46,7 @@
 import '@/assets/css/ball.css'
 import '@/assets/css/run.css'
 import '@/assets/css/style.css'
-// import { ref,onMounted,onBeforeUnmount } from 'vue'
+import { ref,onMounted,onBeforeUnmount } from 'vue'
 
 // import axios from 'axios';
 export default {
@@ -60,121 +54,125 @@ export default {
   components: {
   },
   setup() {
-    // let drawResult = ref('')
-    // let cycleNo = 0
-    // let openStatus = true
-    // let fallTimes = 0
-    // let messageArr = ref([])
-    // let runBallStatus = ref(false)
-    // let timeout8 = ref(null)
-    // let messageStatus = ref(false)
-    // let messageText = ref('')
-    // let fallNum = ref('')
-    // let fallStatus = ref(false)
+    let drawResult = ref('')
+    let cycleNo = 0
+    let openStatus = true
+    let fallTimes = 0
+    let messageArr = ref([])
+    let runBallStatus = ref(false)
+    let timeout8 = ref(null)
+    let messageStatus = ref(false)
+    let messageText = ref('')
+    let fallNum = ref('')
+    let fallStatus = ref(false)
 
-    // // 拿開獎數字
-    // const getNum = async() => {
-    //     await fetch('https://globalcaipiaokong.com/api/trial/draw-result?code=twklb&rows=1', {
-    //         headers: {
-    //             'content-type': 'application/json' // 這一欄一定要設定！
-    //         },
-    //         method: 'GET',
-    //     })
-    //     .then(response => response.json()) // 輸出成 json
-    //     .then(res => {
+    // 拿開獎數字
+    const getNum = async() => {
+        await fetch('https://globalcaipiaokong.com/api/trial/draw-result?code=twklb&rows=1', {
+            headers: {
+                'content-type': 'application/json' // 這一欄一定要設定！
+            },
+            method: 'GET',
+        })
+        .then(response => response.json()) // 輸出成 json
+        .then(res => {
 
-    //         if(res.msg === '成功') {
-    //             if(cycleNo === res.data[0].cycleNo) {
-    //                 openStatus = false
-    //             }else{
-    //                 cycleNo = res.data[0].cycleNo
-    //                 openStatus = true
-    //             }
+            if(res.msg === '成功') {
+                if(cycleNo === res.data[0].cycleNo) {
+                    openStatus = false
+                }else{
+                    cycleNo = res.data[0].cycleNo
+                    openStatus = true
+                }
                 
-    //             drawResult.value = res.data[0].drawResult
-    //             messageArr.value = res.data[0].drawResult.split(',')
-    //         }
+                drawResult.value = res.data[0].drawResult
+                messageArr.value = res.data[0].drawResult.split(',')
+            }
 
-    //     }).catch((error) => {
-    //         console.error("Error:", error)
-    //     })
-    // }
-    // //掉球動作
-    // const fallBall = () => {
-    //     if(fallTimes<20){
-    //         setTimeout(function (){
-    //             // 掉球
-    //             let text = messageArr.value.slice(0,fallTimes+1).join(',')
-    //             controlMessage(true,text,cycleNo)
+        }).catch((error) => {
+            console.error("Error:", error)
+        })
+    }
+    //掉球動作
+    const fallBall = () => {
+        if(fallTimes<20){
+            setTimeout(function (){
+                // 掉球
+                let text = messageArr.value.slice(0,fallTimes+1).join(',')
+                controlMessage(true,text,cycleNo)
 
-    //             fallNum.value = drawResult.value.split(',')[fallTimes]
-    //             fallStatus.value = true
-    //             setTimeout(function (){
-    //                 // 收球
-    //                 fallStatus.value = false
-    //                 fallTimes++
-    //                 fallBall()
-    //             },150)
-    //         },50)
-    //     }else {
-    //         controlRunBall(false)
-    //     }
-    // }
-    // //控制開獎視窗
-    // const controlMessage = (status = false,text = '',title = '') => {
-    //     messageStatus.value = status
-    //     if(title) {
-    //         messageText.value = '編號'+title+":"+"<br>"+text
-    //     } else {
-    //         messageText.value = text
-    //     }
-    // }
-    // // 控制滾球動畫
-    // let controlRunBall = (status = false) => {
-    //     runBallStatus.value = status
-    // }
-    // // 開獎動作
-    // const play = () => {
-    //     fallTimes = 0
-    //     // 掉球和結果動畫
-    //     fallBall()
-    // }
-    // // 關閉獎項視窗
-    // const close = () => {
-    //     messageStatus.value = false
-    // }
-    // //初始動作
-    // const init = async() => {
-    //     await getNum()
-    // }
-    // init()
+                fallNum.value = drawResult.value.split(',')[fallTimes]
+                fallStatus.value = true
+                setTimeout(function (){
+                    // 收球
+                    fallStatus.value = false
+                    fallTimes++
+                    fallBall()
+                },150)
+            },50)
+        }else {
+            controlRunBall(false)
+        }
+    }
+    //控制開獎視窗
+    const controlMessage = (status = false,text = '',title = '') => {
+        messageStatus.value = status
+        if(title) {
+            messageText.value = '編號'+title+":"+"<br>"+text
+        } else {
+            messageText.value = text
+        }
+    }
+    // 控制滾球動畫
+    let controlRunBall = (status = false) => {
+        runBallStatus.value = status
+    }
+    // 開獎動作
+    const play = () => {
+        fallTimes = 0
+        // 掉球和結果動畫
+        fallBall()
+    }
+    // 關閉獎項視窗
+    const close = () => {
+        messageStatus.value = false
+    }
+    //初始動作
+    const init = async() => {
+        await getNum()
+    }
+    init()
 
-    // onMounted(() => {
-    //     setTimeout(function (){
-    //         controlMessage(true,drawResult.value,cycleNo)
-    //     },1500)
+    onMounted(() => {
+        setTimeout(function (){
+            controlMessage(true,drawResult.value,cycleNo)
+        },1500)
 
-    //     timeout8.value = window.setInterval((async() => {
-    //         await getNum()
-    //         if(openStatus) {
-    //             controlRunBall(true)
-    //             play()
-    //         }
-    //     } ), 10000)
-    // })
+        // timeout8.value = window.setInterval((async() => {
+        //     await getNum()
+        //     if(openStatus) {
+        //         controlRunBall(true)
+        //         play()
+        //     }
+        // } ), 10000)
 
-    // onBeforeUnmount(() => {
-    //     clearInterval(timeout8.value)
-    // })
+        // setTimeout(function (){
+        //    play()
+        // },2500)
+    })
 
-    // return {
-    //     runBallStatus,
-    //     messageStatus,
-    //     messageText,
-    //     fallNum,
-    //     fallStatus,
-    //     close
-    // }
+    onBeforeUnmount(() => {
+        clearInterval(timeout8.value)
+    })
+
+    return {
+        runBallStatus,
+        messageStatus,
+        messageText,
+        fallNum,
+        fallStatus,
+        close
+    }
 
   }
 }
