61a5a5d915448654d3a5509bf7c732230c62aafe
jxes6102 <jxes6102@gmail.com>
2023-04-26 15:25:45 +0800

    temp04261526

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="w-[100vw] h-[100vh] bg-[#fcfce5] flex flex-wrap justify-center items-center md:gap-[20px]">
-      <div class="w-[50%] md:w-auto h-auto flex flex-wrap justify-center items-center">
+  <div class="w-[100vw] h-[100vh] bg-[#fcfce5] flex flex-wrap justify-center items-center flex-col md:flex-row gap-[20px]">
+      <div class="w-[100%] md:w-auto md:h-auto flex flex-wrap justify-center items-center">
         <div class="w-[80px] md:w-[100px] text-base md:text-2xl bg-[#8ac6d1] px-2 py-1 rounded-[5px] cursor-pointer hover:opacity-80" @click="toLinkBar">bar</div>
       </div>
-      <div class="w-[50%] md:w-auto h-auto flex flex-wrap justify-center items-center">
+      <div class="w-[100%] md:w-auto md:h-auto flex flex-wrap justify-center items-center">
         <div class="w-[80px] md:w-[100px] text-base md:text-2xl bg-[#8ac6d1] px-2 py-1 rounded-[5px] cursor-pointer hover:opacity-80" @click="toLinkCapsule">capsule</div>
       </div>
-      <div class="w-[50%] md:w-auto h-auto flex flex-wrap justify-center items-center">
+      <div class="w-[100%] md:w-auto md:h-auto flex flex-wrap justify-center items-center">
         <div class="w-[80px] md:w-[100px] text-base md:text-2xl bg-[#8ac6d1] px-2 py-1 rounded-[5px] cursor-pointer hover:opacity-80" @click="toLinkHistory">history</div>
       </div>
-      <div class="w-[50%] md:w-auto h-auto flex flex-wrap justify-center items-center">
+      <div class="w-[100%] md:w-auto md:h-auto flex flex-wrap justify-center items-center">
         <div class="w-[80px] md:w-[100px] text-base md:text-2xl bg-[#8ac6d1] px-2 py-1 rounded-[5px] cursor-pointer hover:opacity-80" @click="toLinkBox">box</div>
       </div>
   </div>
