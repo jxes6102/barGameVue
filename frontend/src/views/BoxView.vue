bc155646567a263a49529180e8239c820844ce8e
jxes6102 <jxes6102@gmail.com>
2023-04-28 10:32:44 +0800

    temp04281034

--- frontend/src/views/BoxView.vue
+++ frontend/src/views/BoxView.vue
@@ -33,7 +33,7 @@
                     <span
                         v-for="(item,index) in 80" :key="index"
                         class="z-[2] w-[15px] h-[15px] md:w-[40px] md:h-[40px] bg-contain bg-center bg-no-repeat flex flex-wrap justify-center items-center text-[12px] md:text-lg"
-                        :class="(item === 4)? ('ball-' + ((item%4)+1) + ' wieyi_'+item) : ''"
+                        :class="(item <= 20)? ('ball-' + ((item%4)+1) + ' wieyi_'+item) : ''"
                     >
                         {{ item }}
                     </span>
