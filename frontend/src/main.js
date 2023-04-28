b0d388b7cfec4c201748d7dd2bce0eacbc71da3b
jxes6102 <jxes6102@gmail.com>
2023-04-28 11:57:37 +0800

    temp04281159

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,14 @@ import './assets/tailwind.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import twCn from 'element-plus/dist/locale/zh-tw.mjs'
 
 
 const app = createApp(App)
+
+app.use(ElementPlus, {
+    locale: twCn,
+})
 
 app.use(store).use(router).use(ElementPlus).mount('#app')
 
