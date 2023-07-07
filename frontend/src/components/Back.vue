f4d337a0bed7b4fd48303d843ffa3003999b87c8
jxes6102 <jxes6102@gamil.com>
2023-07-07 16:06:01 +0800

    temp07071606

--- frontend/src/components/Back.vue
+++ frontend/src/components/Back.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 left-0 cursor-pointer" @click="toLinkBack">
-        <el-icon :size="40"><Back /></el-icon>
+        <el-icon :size="40" color="#fc3a52"><Back /></el-icon>
     </div>
 </template>
 <script>
