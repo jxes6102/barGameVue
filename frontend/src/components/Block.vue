9fec6b5b8246723988fe0c38c34d1154aa103cf2
jxes6102 <jxes6102@gamil.com>
2024-02-01 15:50:21 +0800

    advertisement - 5

--- frontend/src/components/Block.vue
+++ frontend/src/components/Block.vue
@@ -7,19 +7,9 @@
         ></div>
     </div>
     <div v-else-if="(!drawStatus) && (type==='all')"
-        @click.self="open" 
+        @click.self="link" 
         class="absolute w-[100%] h-[100%] bg-[rgb(65,65,65,0.7)] text-2xl text-gray-500 font-extrabold bg-[white] flex flex-wrap justify-center items-center">
         {{t('advertisement')}}
-        <Teleport to="body">
-            <div
-                @click.self="close" 
-                v-if="status" 
-                class="fixed w-[100%] h-[100%] top-0 left-0 bg-[rgb(65,65,65,0.7)] flex justify-center items-center z-[223] ">
-                <div class="w-[90vw] h-[90vw] md:w-[90vh] md:h-[90vh]">
-                    <iframe class="w-full h-full" :src="url" frameborder="0"></iframe>
-                </div>
-            </div>
-        </Teleport>
     </div>
     
     <!-- <div v-else-if="(!drawStatus) && (type==='all')" class="absolute w-[100%] h-[100%] flex justify-center items-center">
@@ -50,21 +40,10 @@ export default {
   setup() {
     const { t } = useI18n()
     const url = ref('https://tw.yahoo.com/')
-    const status = ref(false)
-    const open = () => {
-        status.value = true
-    }
-    const close = () => {
-        status.value = false
-    }
     const link = () => {
-        window.open('https://www.google.com.tw/')
+        window.open(url.value)
     }
     return {
-        url,
-        status,
-        open,
-        close,
         link,
         t
     }
--- frontend/src/components/advertisement.vue
+++ frontend/src/components/advertisement.vue
@@ -2,23 +2,13 @@
     <div class="w-[100vw] h-auto">
         <div class="w-full h-[10vh] md:h-[20vh]"></div>
         <div
-            @click="open"  
+            @click="link"
             class="fixed bottom-0 w-[100vw] h-[auto] flex flex-wrap justify-center items-center z-[222]">
             <div
                 class="w-[100vw] h-[15vh] text-2xl text-gray-500 font-extrabold bg-[white] flex flex-wrap justify-center items-center">
                 {{t('advertisement')}}
             </div>
         </div>
-        <Teleport to="body">
-            <div
-                @click.self="close" 
-                v-if="status" 
-                class="fixed w-[100%] h-[100%] top-0 left-0 bg-[rgb(65,65,65,0.7)] flex justify-center items-center z-[223] ">
-                <div class="w-[90vw] h-[90vw] md:w-[90vh] md:h-[90vh]">
-                    <iframe class="w-full h-full" :src="url" frameborder="0"></iframe>
-                </div>
-            </div>
-        </Teleport>
     </div>
 </template>
 
@@ -34,22 +24,11 @@ export default {
     setup() {
         const { t } = useI18n()
         const url = ref('https://tw.yahoo.com/')
-        const status = ref(false)
-        const open = () => {
-            status.value = true
-        }
-        const close = () => {
-            status.value = false
-        }
         const link = () => {
-            window.open('https://www.google.com.tw/')
+            window.open(url.value)
         }
         return {
-            url,
-            status,
             t,
-            open,
-            close,
             link
         }
 
